00bd6f8d8d5db5101c90be0224106851345cbe16
gmz <guomingzhi@zzdwlp.com>
2023-03-02 21:56:54 +0800

    feat: 增加stylelint验证

---
+++ .stylelintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  extends: [
+    'stylelint-config-standard', // 配置stylelint拓展插件
+    'stylelint-config-prettier', // 配置stylelint和prettier兼容
+    'stylelint-config-recess-order', // 配置stylelint css属性书写顺序插件
+    'stylelint-config-standard-scss' // 配置stylelint scss插件
+  ],
+  plugins: ['stylelint-less'],
+  rules: {
+    'at-rule-no-unknown': null,
+    'scss/at-rule-no-unknown': null,
+    'color-no-invalid-hex': true,
+    'less/color-no-invalid-hex': true
+  }
+};
