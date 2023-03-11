b829b7f4793868a3d9bbfc4f9c927b852f2c9e0e
gmz <guomingzhi@zzdwlp.com>
2023-03-11 20:17:51 +0800

    feat: 增加copy-webpack-plugin，增加readme.md

---
+++ README.md
@@ -0,0 +1,26 @@
+# webpack-react-demo
+
+> 本项目由 react typescript webpack 搭建
+
+基本功能涉及
+
+- less 预编译语法支持
+- css-module
+- 打包样式自动添加浏览器前缀
+- react-router-dom 6 版本内置集中式路由
+
+项目优化方面涉及
+
+- js css 代码压缩
+- 公共文件提取
+- 多进程构建、压缩
+- webpack5 内置长期缓存加快构建速度
+
+代码规范涉及
+
+- eslint 检查
+- ts 类型检查
+- css 样式编写顺序检查
+- commit message 检查
+
+项目仅供学习交流使用。
--- build/webpack.prod.js
+++ build/webpack.prod.js
@@ -1,54 +1,67 @@
-const webpack = require("webpack");
-const path = require("path");
-const glob = require("glob");
-const { merge } = require("webpack-merge");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
-const TerserPlugin = require("terser-webpack-plugin");
-const { PurgeCSSPlugin } = require("purgecss-webpack-plugin");
-const baseConfig = require("./webpack.base");
+const webpack = require('webpack');
+const path = require('path');
+const glob = require('glob');
+const { merge } = require('webpack-merge');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const { PurgeCSSPlugin } = require('purgecss-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const baseConfig = require('./webpack.base');
 
 const PATHS = {
-  src: path.join(__dirname, "../src"),
+  src: path.join(__dirname, '../src')
 };
 
 module.exports = merge(baseConfig, {
-  mode: "production",
+  mode: 'production',
   plugins: [
     new MiniCssExtractPlugin({
-      filename: "[name]_[contenthash:8].css",
+      filename: '[name]_[contenthash:8].css'
     }),
     // purgecss-webpack-plugin遇到css-module基本已失效，后续再找下解决方法
     new PurgeCSSPlugin({
       paths: glob.sync(`${PATHS.src}/**/*`, { nodir: true }),
-      defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
+      defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
       safelist: {
-        deep: [/css__module__/],
-      },
+        deep: [/css__module__/]
+      }
     }),
+    // 复制文件插件
+    new CopyPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, '../public'), // 复制public下文件
+          to: path.resolve(__dirname, '../dist'), // 复制到dist目录中
+          filter: source => {
+            return !source.includes('index.html'); // 忽略index.html
+          }
+        }
+      ]
+    })
   ],
   optimization: {
     minimizer: [
       new TerserPlugin({
-        parallel: true,
+        parallel: true
       }),
-      new CssMinimizerPlugin(),
+      new CssMinimizerPlugin()
     ],
     splitChunks: {
       cacheGroups: {
         vendors: {
           test: /(react|react-dom)/,
-          name: "vendors",
-          chunks: "all",
+          name: 'vendors',
+          chunks: 'all'
         },
         commons: {
           // 提取页面公共代码
-          name: "commons", // 提取文件命名为commons
+          name: 'commons', // 提取文件命名为commons
           minChunks: 2, // 只要使用两次就提取出来
-          chunks: "initial", // 只提取初始化就能获取到的模块，不管异步的
-          minSize: 0, // 提取代码体积大于0就提取出来
-        },
-      },
-    },
-  },
+          chunks: 'initial', // 只提取初始化就能获取到的模块，不管异步的
+          minSize: 0 // 提取代码体积大于0就提取出来
+        }
+      }
+    }
+  }
 });
